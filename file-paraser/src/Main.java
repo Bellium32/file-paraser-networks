193041afc94d8baed0d0498fac80a1421cfe1d35
Bellium32 <116981843+Bellium32@users.noreply.github.com>
2024-05-14 14:48:28 +0100

    Add files via upload

--- file-paraser/src/Main.java
+++ file-paraser/src/Main.java
@@ -6,28 +6,27 @@ class Main
 {
     public static void main(String args[])
     {
-        //Ping_Parase("camPi1.log", "newLog.txt", "newLog2.txt");
-        //Ping_Parase("ethzPi1.log", "ethzPiLog.txt", "ethzPacketLog.txt");
-        //Median_Parase("camPi1.log", "newLog3.txt");
-        //TraceRoute_Parase("camTrace1.log", "newLog4.txt");
-        TPut_Parase("bigFileDown1.log", "newLog5.txt");
+        Ping_Parase("", "", "");
+        Median_Parase("", "");
+        TraceRoute_Parase2("", "");
     }
 
+    //Finds and outputs the min/avg/max/mdev rtt and the packet loss and outputs them into seperate CSV files
     public static void Ping_Parase(String toRead, String pingWrite, String packetWrite){
     try{
         //Opens the named file
-        //Name the file you want to read from here
+
         FileInputStream readIn = new FileInputStream(toRead);
         // Get the object of DataInputStream
         DataInputStream in = new DataInputStream(readIn);
         //Buffer for the input
         BufferedReader bufferLine = new BufferedReader(new InputStreamReader(in));
 
-        //Name the file you want to write pings to
+
         FileWriter outPing = new FileWriter(pingWrite);
         BufferedWriter pingLogging = new BufferedWriter(outPing);
 
-        //Name the file you want to write packet loss to
+
         FileWriter outLoss = new FileWriter(packetWrite);
         BufferedWriter lossLogging = new BufferedWriter(outLoss);
         String txtLine;
@@ -49,7 +48,7 @@ class Main
                 pingLogging.write("\n");
             }
             if (txtLine.toLowerCase().contains(matchLine2)){
-                dataPoints2 = txtLine.substring(39);
+                dataPoints2 = txtLine.substring(txtLine.indexOf("received")+10);
                 dataLoss = dataPoints2.substring(0, dataPoints2.indexOf(" "));
 
                 lossLogging.write(dataLoss + ",\n");
@@ -66,6 +65,7 @@ class Main
     }
 }
 
+    //Finds all of the time values in the log and then outputs them to be processed in excel
     public static void Median_Parase(String toRead, String meanWrite){
         try{
             //Opens the named file
@@ -92,11 +92,11 @@ class Main
 
                     writeMean = dataPoints.substring(5,9);
 
-                    meanLogging.write(writeMean+",");
+                    meanLogging.write(writeMean+"\n");
 
                 }
                 if (txtLine.toLowerCase().contains(matchLine2)){
-                    meanLogging.write("\n");
+                    meanLogging.write(",");
                 }
 
 
@@ -109,6 +109,7 @@ class Main
         }
 }
 
+    // Gives named adress aswell
     public static void TraceRoute_Parase(String toRead, String traceWrite){
         try{
             //Opens the named file
@@ -152,7 +153,52 @@ class Main
             System.err.println("Error: " + e.getMessage());
         }
     }
+    // Only gives adress as IP
+    public static void TraceRoute_Parase2(String toRead, String traceWrite){
+        try{
+            //Opens the named file
+            FileInputStream readIn = new FileInputStream(toRead);
+            // Get the object of DataInputStream
+            DataInputStream in = new DataInputStream(readIn);
+            //Buffer for the input
+            BufferedReader bufferLine = new BufferedReader(new InputStreamReader(in));
 
+            FileWriter outTrace = new FileWriter(traceWrite);
+            BufferedWriter traceLogging = new BufferedWriter(outTrace);
+
+            String txtLine;
+            //Checks to see if line matches data being looked for
+            String matchLine= "traceroute to";
+
+            String dataPoints;
+
+            //Read File Line By Line
+            while ((txtLine = bufferLine.readLine()) != null)   {
+                // Print the content on the console
+                if (txtLine.toLowerCase().contains(matchLine) == false){
+                    if (txtLine.toLowerCase().contains("* * *")) {
+                        traceLogging.write(txtLine + ", ");
+                    }
+                    else{
+                        dataPoints = txtLine.substring(txtLine.indexOf("("), txtLine.indexOf(")") + 1);
+                        traceLogging.write(dataPoints + ", ");
+                    }
+                }
+                if (txtLine.toLowerCase().contains(matchLine)){
+                    traceLogging.write("\n");
+                }
+
+
+            }
+            traceLogging.close();
+            //Close the input stream
+            in.close();
+        }catch (Exception e){//Catch exception if any
+            System.err.println("Error: " + e.getMessage());
+        }
+    }
+
+    //Finds and output the Throughput (transferspeed) of the given file
     public static void TPut_Parase(String toRead, String tPutWrite){
         try{
             //Opens the named file
